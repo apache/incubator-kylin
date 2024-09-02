964a6086de87c6d37abe25d3d964f764fce2e475
ShengHuang <huangshengtx@163.com>
2024-09-02 14:40:30 +0800

    KYLIN-5959 Support internal table for website

--- kystudio/src/locale/en.js
+++ kystudio/src/locale/en.js
@@ -495,6 +495,7 @@ exports.default = {
     datasource: 'Data Source',
     source: 'Data Source',
     modelList: 'Model',
+    internalTable: 'Internal Table',
     index: 'Index',
     modeledit: 'Model Edit',
     snapshot: 'Snapshot',
