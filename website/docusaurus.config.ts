2e5eda7e13320b5544e3e523ab319e596052c1ea
jlf <1251489546@qq.com>
2024-10-22 16:07:09 +0800

    add i18n and use `docusaurus-plugin-search-local` replace `@cmfcmf/docusaurus-search-local`

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -48,7 +48,7 @@ function getAnnouncedVersion() {
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Apache Kylin',
-  tagline: 'Kylin is a high concurrency, high performance and intelligent OLAP engine that provides low-cost and ultimate data analytics experience  .',
+  tagline: 'Kylin is a high concurrency, high performance and intelligent OLAP engine that provides low-cost and ultimate data analytics experience.',
   url: 'https://kylin.apache.org',
   baseUrl: '',
   onBrokenLinks: 'warn',
@@ -65,20 +65,11 @@ const config = {
 
   plugins: [
     [
-      require.resolve("@cmfcmf/docusaurus-search-local"),
+      require.resolve("docusaurus-plugin-search-local"),
       {
         // Options here
         // whether to index docs pages
         indexDocs: true,
-
-        // Whether to also index the titles of the parent categories in the sidebar of a doc page.
-        // 0 disables this feature.
-        // 1 indexes the direct parent category in the sidebar of a doc page
-        // 2 indexes up to two nested parent categories of a doc page
-        // 3...
-        //
-        // Do _not_ use Infinity, the value must be a JSON-serializable integer.
-        indexDocSidebarParentCategories: 0,
 
         // whether to index blog pages
         indexBlog: true,
@@ -86,52 +77,6 @@ const config = {
         // whether to index static pages
         // /404.html is never indexed
         indexPages: false,
-
-        // language of your documentation, see next section
-        language: "en",
-
-        // setting this to "none" will prevent the default CSS to be included. The default CSS
-        // comes from autocomplete-theme-classic, which you can read more about here:
-        // https://www.algolia.com/doc/ui-libraries/autocomplete/api-reference/autocomplete-theme-classic/
-        // When you want to overwrite CSS variables defined by the default theme, make sure to suffix your
-        // overwrites with `!important`, because they might otherwise not be applied as expected. See the
-        // following comment for more information: https://github.com/cmfcmf/docusaurus-search-local/issues/107#issuecomment-1119831938.
-        style: undefined,
-
-        // The maximum number of search results shown to the user. This does _not_ affect performance of
-        // searches, but simply does not display additional search results that have been found.
-        maxSearchResults: 8,
-
-        // lunr.js-specific settings
-        lunr: {
-          // When indexing your documents, their content is split into "tokens".
-          // Text entered into the search box is also tokenized.
-          // This setting configures the separator used to determine where to split the text into tokens.
-          // By default, it splits the text at whitespace and dashes.
-          //
-          // Note: Does not work for "ja" and "th" languages, since these use a different tokenizer.
-          tokenizerSeparator: /[\s\-]+/,
-          // https://lunrjs.com/guides/customising.html#similarity-tuning
-          //
-          // This parameter controls the importance given to the length of a document and its fields. This
-          // value must be between 0 and 1, and by default it has a value of 0.75. Reducing this value
-          // reduces the effect of different length documents on a term’s importance to that document.
-          b: 0.75,
-          // This controls how quickly the boost given by a common word reaches saturation. Increasing it
-          // will slow down the rate of saturation and lower values result in quicker saturation. The
-          // default value is 1.2. If the collection of documents being indexed have high occurrences
-          // of words that are not covered by a stop word filter, these words can quickly dominate any
-          // similarity calculation. In these cases, this value can be reduced to get more balanced results.
-          k1: 1.2,
-          // By default, we rank pages where the search term appears in the title higher than pages where
-          // the search term appears in just the text. This is done by "boosting" title matches with a
-          // higher value than content matches. The concrete boosting behavior can be controlled by changing
-          // the following settings.
-          titleBoost: 5,
-          contentBoost: 1,
-          tagsBoost: 3,
-          parentCategoriesBoost: 2, // Only used when indexDocSidebarParentCategories > 0
-        }
       },
     ],
   ],
@@ -141,7 +86,19 @@ const config = {
   // to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: 'en',
-    locales: ['en'],
+    // locales: ['en'],
+    locales: ['en', 'zh-Hans'],
+    // path: 'i18n',
+    localeConfigs: {
+    //   'en': {
+    //     label: 'English',
+    //     htmlLang: 'en-GB',
+    //   },
+      'zh-Hans': {
+        label: '简体中文',
+        path: 'zh-Hans',
+      },
+    },
   },
 
   presets: [
@@ -268,6 +225,10 @@ const config = {
             docId: 'development/intro',
             position: 'left',
             label: 'Development',
+          },
+          {
+            type: 'localeDropdown',
+            position: 'right',
           },
           {
             type: 'doc',
--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -6,24 +6,25 @@ import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import styles from './index.module.css';
 
+import Translate, {translate} from '@docusaurus/Translate';
 
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
   return (
         <header className={clsx(styles.heroBanner)}>
             <div className={styles.container}>
-                <h1 className={styles.title}>Smarter and Faster</h1>
-                <p className={styles.subtitle}>{siteConfig.tagline}</p>
+                <h1 className={styles.title}><Translate>Smarter and Faster</Translate></h1>
+                <p className={styles.subtitle}><Translate>{siteConfig.tagline}</Translate></p>
                 <br/>
                 <div>
                   <Link className={clsx("button button--lg button--secondary", styles.buttons)}
                     to="/docs/overview">
-                      What's New
+                      <Translate>What's New</Translate>
                   </Link>
                     <span>&emsp;&emsp;&emsp;</span>
                   <Link className={clsx("button button--lg button--secondary", styles.buttons1)}
                     to="/docs/quickstart/intro">
-                      Play in Docker
+                      <Translate>Play in Docker</Translate>
                     </Link>
                 </div>
             </div>
